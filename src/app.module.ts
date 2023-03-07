52cecbb86af31f0bd241a7b8a46330876c3c9c1d
Mirete Benoit <ben@mirete.fr>
2023-03-07 21:49:25 +0100

    feat: galleries & photos modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { GalleriesModule } from './galleries/galleries.module';
+import { PhotosModule } from './photos/photos.module';
 
 @Module({
   imports: [
@@ -17,6 +19,8 @@ import { CommonModule } from './common/common.module';
     ClientsModule,
     UsersModule,
     CommonModule,
+    GalleriesModule,
+    PhotosModule,
   ],
   controllers: [],
   providers: [],
